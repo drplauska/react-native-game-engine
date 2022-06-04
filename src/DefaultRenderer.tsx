df4073e83f4f680118d6e6007d6badc933618693
Ernestas Plauska <Ernestas_Plauska@epam.com>
2022-06-04 07:11:16 +0300

    remove redundant code as new verified as working

--- src/DefaultRenderer.tsx
+++ src/DefaultRenderer.tsx
@@ -41,29 +41,4 @@ export default (
     });
 
   return entitiesToRender;
-
-  return Object.keys(entities)
-    .filter((key) => entities[key].renderer)
-    .map((key) => {
-      let entity = entities[key];
-      if (typeof entity.renderer === "object") {
-        return (
-          <entity.renderer.type
-            key={key}
-            screen={screen}
-            layout={layout}
-            {...entity}
-          />
-        );
-      } else if (typeof entity.renderer === "function") {
-        return (
-          <entity.renderer
-            key={key}
-            screen={screen}
-            layout={layout}
-            {...entity}
-          />
-        );
-      }
-    });
 };
